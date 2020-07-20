78b410550b84f3ef1fa379b8ceaa636691a3659d
ternlef11 <aliarazzak@hotmail.com>
2020-07-20 13:06:08 +0200

    css +redirect

--- src/collection.js
+++ src/collection.js
@@ -10,7 +10,7 @@ export default class Collection extends Component {
     constructor(props) {
     super(props);
     this.state = {
-      newheadline : <div className="headlinebox"><p className="headlinetext">Looks better on desktop :)</p></div>,
+      newheadline : <div className="headlinebox"><p className="headlinetext">Looks better on desktop or laptop :)</p></div>,
       thewords: ["THERE WAS A LIGHT AT THE END OF THIS TUNNEL", "MOURNING GOWN & ALL", "WITH A GOD DAMN FROWN", "& HERE I THOUGHT IT WAS ALL FUN & GAMES", "ANOTHER NIGHT SPENT BY CANDLE LIGHT", "A QUICK STROLL IN THE RAIN", "LOVE PILES OF FALLEN LEAVES & WET GRASS", "SPEAK UP - SAY WHAT - NOT SURE - ANOTHER DAY", "HARD TO REMEMBER & EASY TO FORGET", "LIKE AN ALLEY WHERE REFUSE IS DISCARDED", "HALLOWED GROUND FOR THOSE WHO WANT, FOR THOSE WHO NEED", "DOVES OF A FLOCK ALL LOOK THE SAME SPICED & STEWED", "LONG NIGHTS SPENT, LONG NIGHTS PENT, LONG NIGHTS NEED A TENT", "IF YOU ASK ME ETERNITY WOULD HAVE BEEN KINDER", "LOOKING FOR KIND & RESERVED CATS : CAN PROVIDE COMPANY", "HAIR LIKE PIANO WIRE, FACE LIKE WORN TYRES"],
 
     }
