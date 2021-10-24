f0cbbe604bc95581c7e24022066e10ef35f31b79
Ali Razzak <aliarazzak@hotmail.com>
2021-10-24 17:15:40 +0200

    Added PDF :japanese_goblin:

--- src/components/tocsb.js
+++ src/components/tocsb.js
@@ -7,11 +7,11 @@ export default class TOCsb extends Component {
         <div className="portdescwrap">
           <div className="portdesc">
             <p className="portdesctitle">Tools of Relation</p>
-            <p className="portdesctext">Developed & Designed app in collaboration with Lila Bullen-Smith & Naira Nigrelli to map Rivelt academy staff network. App designed in PyQt5 with MongoDB database.</p>
+            <p className="portdesctext">Developed & Designed app commissioned by Lila Bullen-Smith & Naira Nigrelli to map Rivelt Academy staff network. App designed in PyQt5 with MongoDB database.</p>
           </div>
         </div>
         <div className="portlinkwrap">
-          <a href="www.google.com" target="_blank" rel="noopener noreferrer"><p className="linktext">PDF</p></a>
+          <a href="https://www.dropbox.com/s/f0uczmazf96xvvx/sb_final_project.pdf?dl=0" target="_blank" rel="noopener noreferrer"><p className="linktext">PDF</p></a>
         </div>
       </div>
   )
