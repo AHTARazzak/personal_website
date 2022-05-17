0b6d17f169116857ab48d5408f8c5f5f7e0b9f98
Ali Razzak <aliarazzak@hotmail.com>
2022-05-17 18:30:36 +0200

    Added to Threadbare

--- src/components/threadbare.js
+++ src/components/threadbare.js
@@ -10,6 +10,9 @@ export default class RickOF extends Component {
             <p className="portdesctext">Bot that dynamically scrapes several secondhand vendors for luxury brands & updates new listings to Discord.</p>
           </div>
         </div>
+        <div className="portlinkwrap">
+          <a href="https://discord.gg/YSsK4qtU" target="_blank" rel="noopener noreferrer"><p className="linktext">Discord</p></a>
+        </div>
       </div>
   )
 }
